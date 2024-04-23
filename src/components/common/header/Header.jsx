8cfb022dfde04c6c680b98d87ec408d46f898c65
abhinavcodedeveloper <abhinavssr2003@gmail.com.com>
2024-04-23 06:23:17 +0530

    Contract done

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -28,9 +28,9 @@ const Header = () => {
             <h4>
               <span>2</span> My List
             </h4>
-            <button className='btn1'>
-              <i className='fa fa-sign-out'></i> Sign In
-            </button>
+            <a className='btn1' href="https://wie-olive.vercel.app  ">
+              <i className='fa fa-sign-out'></i> CONTRACT
+            </a>
           </div>
 
           <div className='toggle'>
